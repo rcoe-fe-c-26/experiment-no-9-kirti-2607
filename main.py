133a59db6054ad31b91e604e2e4a8bdc431b88b3
kirti-2607 <kirtisawant2611@gmail.com>
2026-01-30 19:08:55 +0530

    Refactor factorial program with updated metadata

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder:Kirti
+# Date:30/01/2026
 
+# Write your code here
 print("--- Factorial Finder ---\n")
-N = int(input("Enter Number:"))
+N = int(input("Enter Number: "))
 a=1
 for x in range(1,N+1):
     a *= x
 print("Factorial of",N,"is",a)
 
 
-# Write your code here
+
+
 
 
