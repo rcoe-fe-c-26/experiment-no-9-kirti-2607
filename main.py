5aa1801ae77bb714a53b78ac95f978eeaf9718e4
kirti-2607 <kirtisawant2611@gmail.com>
2026-01-30 19:02:13 +0530

    Implement factorial calculation with user input
    Added user input for number and implemented factorial calculation.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 # Date:
 
 print("--- Factorial Finder ---\n")
+N = int(input("Enter Number: "))
+a=1
+for x in range(1,N+1):
+    a *= x
+print("Factorial of",N,"is",a)
 
 
 # Write your code here
+
